a5e09cd156a6603fd361bebc01a6afbb8ba62e21
Omar El Houmadi <oel-houm@e1r7p3.1337.ma>
2023-05-23 03:40:59 +0100

    fix exit current process error

--- minishell.c
+++ minishell.c
@@ -6,7 +6,7 @@
 /*   By: oel-houm <oel-houm@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/09 22:00:56 by wbouwach          #+#    #+#             */
-/*   Updated: 2023/05/23 02:39:58 by oel-houm         ###   ########.fr       */
+/*   Updated: 2023/05/23 03:06:32 by oel-houm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,18 +17,18 @@ int global_exit;
 int main(int ac, char **av, char **env)
 {
     // initiate_values(T_DATA);
-    (void)av;
     char            *line;
     t_env           *env_list;
     t_redirection   *redirection;
-    t_cmd_data *cmd_data;
+    t_cmd_data      *cmd_data;
     
+    (void)av;
     check_argc(ac);
+    global_exit = 0;
     env_list = create_env_list(env);
     redirection = malloc(sizeof(t_redirection));
-    cmd_data = malloc(sizeof(t_cmd_data) * 1);
+    cmd_data = malloc(sizeof(t_cmd_data));
     line = readline(GREEN"minishell ▸ "WHITE);
-    global_exit = 0;
     while (1)
     {
         if (line)
@@ -36,7 +36,9 @@ int main(int ac, char **av, char **env)
         else
             return (0); // handle signals here
         if (parse(line))
+        {
             execute(line, cmd_data, redirection, env, env_list);
+        }
         line = readline(GREEN"minishell ▸ "WHITE);
     }
     return (global_exit);
